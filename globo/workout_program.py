b0c49337700fb6c466252ae505ec5e41471c3dc9
Michael Martorella <Marto32@users.noreply.github.com>
2022-05-28 00:00:46 -0400

    Update workout_program.py

--- globo/workout_program.py
+++ globo/workout_program.py
@@ -1,3 +1,4 @@
+import datetime
 import workout
 
 MON, TUE, WED, THU, FRI, SAT, SUN = range(7)
@@ -8,4 +9,15 @@ WS4SB = {
     WED: workout.DynamicEffortLowerBody,
     FRI: workout.RepetitionUpperBody,
     SAT: workout.MaxEffortLowerBody,
+}
+
+# r/Fitness beginner routine
+# https://thefitness.wiki/routines/r-fitness-basic-beginner-routine/
+# Switch back and forth by week so order goes A, B, A, B, ...
+WORKOUT_SWITCH = datetime.datetime.today().isocalendar()[1]
+
+BasicStrengthTraining = {
+    TUE: if WORKOUT_SWITCH workout.WorkoutA else workout.WorkoutB,
+    FRI: if WORKOUT_SWITCH workout.WorkoutB else workout.WorkoutA,
+    SUN: if WORKOUT_SWITCH workout.WorkoutA else workout.WorkoutB,
 }
