6ae0f84711c30b46643f27d0276f7554828bd0d7
Michael Martorella <Marto32@users.noreply.github.com>
2022-06-17 11:31:31 -0400

    Update workout_program.py

--- globo/workout_program.py
+++ globo/workout_program.py
@@ -14,7 +14,7 @@ WS4SB = {
 # r/Fitness beginner routine
 # https://thefitness.wiki/routines/r-fitness-basic-beginner-routine/
 # Switch back and forth by week so order goes A, B, A, B, ...
-WORKOUT_SWITCH = datetime.datetime.today().isocalendar()[1]
+WORKOUT_SWITCH = (datetime.datetime.today().isocalendar()[1] % 2) == 0
 
 BasicStrengthTraining = {
     TUE: workout.WorkoutAWithConditioning if WORKOUT_SWITCH else workout.WorkoutBWithConditioning,
